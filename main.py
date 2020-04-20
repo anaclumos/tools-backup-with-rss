687349edb9650a82e097837cea4811e2e3d7ee32
Sunghyun Cho <mail@chosunghyun.com>
2020-04-21 00:17:37 +0900

    Separate getBlogDomain. Requires manual input.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import os
 from RSSReader import RSSReader as RR
 from MDCreator import MDCreator as MC
 import time
+import pprint
 
 start = time.time()
 
 # ------------
-RSSURL = "https://yourblog.com/rss"
+RSSURL = "https://yourblog.com/rss/"
+RSSHome = "https://yourblog.com/"
 RSSDIR = "backups"
 # ------------
 
@@ -14,9 +18,8 @@ RSSDIR = "backups"
 def backup(rssURL, backupDirectory):
     feed = RR(rssURL)
     postlist = feed.parse()
-    blogDomain = feed.getBlogDomain()
     for post in postlist:
-        newMDFile = MC(post, blogDomain)
+        newMDFile = MC(post, RSSHome)
         newMDFile.createFile(backupDirectory)
 
 
@@ -24,5 +27,9 @@ backup(RSSURL, RSSDIR)
 
 total = time.time() - start
 print(
-    "Total Runtime: " + str(int(total / 60)) + "m " + str(round(total % 60, 3)) + "s."
+    "Total Runtime: "
+    + str(int(total / 60))
+    + "m "
+    + str(round(total % 60, 3))
+    + "s."
 )
